b449007e6b7a3edb77c3666bd51c726d19444ae6
acerbastimur <umutacerbastimur@gmail.com>
2020-04-09 18:50:25 +0300

    readme photo updated

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # react-native-ui-challenge
-![enter image description here](https://p67.f3.n0.cdn.getcloudapp.com/items/jkuJdnKk/Image%202019-07-23%20at%2012.34.18%20AM.png)
+<img width="397" alt="Screen Shot 2020-04-09 at 18 49 30" src="https://user-images.githubusercontent.com/33218120/78914261-dbd1e000-7a92-11ea-9b15-f929682b9203.png">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
-import Header from '../components/header/Header';
-import CitiesBar from '../components/citiesBar/CitiesBar';
-import Card from '../components/card/Card';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import Header from "../components/header/Header";
+import CitiesBar from "../components/citiesBar/CitiesBar";
+import Card from "../components/card/Card";
 export default class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Header style={styles.header} />
-                <CitiesBar style={styles.cities} />
-                <ScrollView style={{ flex: 0.4 }}>
-                    <Card source={require("../assets/images/04.png")} />
-                    <Card source={require("../assets/images/05.png")} />
-                    <Card source={require("../assets/images/03.png")} />
-                    <Card source={require("../assets/images/02.png")} />
-                    <Card source={require("../assets/images/01.png")} />
-
-                </ScrollView>
-
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header style={styles.header} />
+        <CitiesBar style={styles.cities} />
+        <ScrollView style={{ flex: 0.4 }}>
+          <Card source={require("../assets/images/04.png")} />
+          <Card source={require("../assets/images/05.png")} />
+          <Card source={require("../assets/images/03.png")} />
+          <Card source={require("../assets/images/02.png")} />
+          <Card source={require("../assets/images/01.png")} />
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F9F9F9',
-        paddingTop: 60
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+    paddingTop: 30,
+  },
 });
